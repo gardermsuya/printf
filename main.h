94ce615b99df857f0b2e6e461300b11077fcea5a
gardermsuya <hildagardrd0812@gmail.com>
2022-10-19 04:06:50 -0700

    header file

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_number(int n);
+int dec_to_binary(int n);
+int _putstring(char *s);
+int print_digit(int n);
+
+#endifi
