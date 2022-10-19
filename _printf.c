7df9a718dc34a504650270bb0d1798c71925ad39
gardermsuya <hildagardrd0812@gmail.com>
2022-10-19 11:32:38 -0700

    put character and string

--- _printf.c
+++ _printf.c
@@ -56,3 +56,40 @@ int no_struct(char c, int count, va_list argu)
 	}
 	return (count);
 }
+
+/**
+ *_printf - our own printf function
+ * @format : A character string, composed of zero of more directives
+ *
+ * Description: write a formated string to the standard output
+ * Return: an integer the number of character printed excluding the null byte
+ * A: if format is null then we return -1
+ * B: as long as the format is not null we increment
+ * C: if the index is not a % then we puts and count++
+ * D: erase the deeeee
+ * E: if it is not a null then we scan the letter. pass it into help func
+ * F: it is probably a null so we return -1
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	int count = 0;
+	va_list argu;
+
+	va_start(argu, format);
+
+	if (!format)/* A */
+		return (-1);
+
+	for (i = 0; format[i]; i++)/* B */
+	{
+		if (format[i] != '%')/* C */
+		{
+			count++;
+			_putchar(format[i]);
+		}
+	}
+	va_end(argu);
+	return (count);
+}
