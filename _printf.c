9ee36b9a9227a80d8443adb5ee00f5df3f1c715f
ndegz <ndegz@example.com>
2022-10-20 14:46:30 -0700

    printf done

--- _printf.c
+++ _printf.c
@@ -1,102 +1,71 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * no_struct - a helper function that is called when theres a %something
- * @c: the character passed after the %
- * @count:the number of count thus far. it will be incremented
- * @argu: the va_list that is passsed to us so we can va_arg it
+ * check_for_specifiers - checks if there is a valid format specifiers
+ * @format: possiple format specifier
  *
- * Description:i did not use struct like everyone else
- * Return: the count totals
- *
- * A: we passed that character after the %. use it as switch condition
- * B:if it is a char, we put char + one count
- * C:if it is a string we check if null, if so then we put null plus 6
- *	we also put string if not null and count the stuff
- * D:if it is an i, do the same as d. so we put i and let it cascade down
- *	if the number is 0 then we add 1 to counts and put a zero
- *	else we just print the number
- * E:if it was a % then we just plus 1 and put the %
- * H:the default is to just print the %letter and yeah...
- *
+ * Return: pointer to valid function or NULL
  */
-int no_struct(char c, int count, va_list argu)
+static int (*check_for_specifiers(const char *format))(va_list)
 {
-	int j;
-	char *s;
-
-	switch (c) /*A*/
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{NULL, NULL}
+	};
+	for (i = 0; p[i].t != NULL; i++)
 	{
-		case 'c': /*B*/
-			j = va_arg(argu, int);
-			count += _putchar(j);
+		if (*(p[i].t) == *format)
+		{
 			break;
-		case 's':
-			s = va_arg(argu, char *);
-			if (!s)
-			{
-				_putchar('(');
-				_putchar('n');
-				_putchar('u');
-				_putchar('l');
-				_putchar('l');
-				_putchar(')');
-				count += 6;
-			}
-			else
-				count += _putstring(s);
-			break;
-		case '%': /*E*/
-			count += _putchar('%');
-			break;
-		default: /*H*/
-			count += 2;
-			_putchar('%');
-			_putchar(c);
+		}
 	}
-	return (count);
+	return (p[i].f);
 }
 
 /**
- *_printf - our own printf function
- * @format : A character string, composed of zero of more directives
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
  *
- * Description: write a formated string to the standard output
- * Return: an integer the number of character printed excluding the null byte
- * A: if format is null then we return -1
- * B: as long as the format is not null we increment
- * C: if the index is not a % then we puts and count++
- * D: erase the deeeee
- * E: if it is not a null then we scan the letter. pass it into help func
- * F: it is probably a null so we return -1
+ * Return: number of chars printed
  */
-
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
-	va_list argu;
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-	va_start(argu, format);
-
-	if (!format)/* A */
+	if (format == NULL)
 		return (-1);
-
-	for (i = 0; format[i]; i++)/* B */
+	va_start(valist, format);
+	while (format[i])
 	{
-		if (format[i] != '%')/* C */
+		for (; format[i] != '%' && format[i]; i++)
 		{
-			count++;
 			_putchar(format[i]);
+			count++;
 		}
-		else if (format[i + 1])/* E */
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
 		{
-			i++;
-			count = no_struct(format[i], count, argu);
+			count += f(valist);
+			i += 2;
+			continue;
 		}
-		else
+		if (!format[i + 1])
 			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
 	}
-	va_end(argu);
+	va_end(valist);
 	return (count);
 }
--- main.h
+++ main.h
@@ -1,15 +1,22 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_number(int n);
-int dec_to_binary(int n);
-int _putstring(char *s);
-int print_digit(int n);
-
-#endif
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+#endif /* MAIN_H */
